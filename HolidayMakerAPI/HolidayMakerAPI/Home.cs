3c413e49fd7bb79a29e4f34b7f4f5351c3a62d12
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2020-09-10 14:00:17 +0200

    Addon

--- HolidayMakerAPI/HolidayMakerAPI/Home.cs
+++ HolidayMakerAPI/HolidayMakerAPI/Home.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,15 @@ namespace HolidayMakerAPI
     public class Home
     {
         public int HomeId { get; set; }
+        [StringLength(50)]
         public string HomeType { get; set; }
         public int Rooms { get; set; }
+        [StringLength(50)]
         public string Location { get; set; }
         public decimal Price { get; set; }
         public bool HasBalcony { get; set; }
         public bool HasWifi { get; set; }
+        [StringLength(1000)]
         public string Image { get; set; }
         public bool HasHalfPension { get; set; }
         public bool HasFullPension { get; set; }
@@ -24,6 +28,7 @@ namespace HolidayMakerAPI
         public int NumberOfBeds { get; set; }
         public bool HasPool { get; set; }
         public bool AllowSmoking { get; set; }
+        [StringLength(3000)]
         public string Description { get; set; }
         public bool AllowPets { get; set; }
         public int NumberOfRatings { get; set; }
