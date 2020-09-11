fbfb324ab3263114177b3246c1a8ca2721b9f5cf
mikaelollhage <mikaelollhage@gmail.com>
2020-09-11 09:08:22 +0200

    Changed UserID to UserId in User model

--- HolidayMakerAPI/HolidayMakerAPI/Models/User.cs
+++ HolidayMakerAPI/HolidayMakerAPI/Models/User.cs
@@ -7,7 +7,7 @@ namespace HolidayMakerAPI
 {
     public class User
     {
-        public int UserID { get; set; }
+        public int UserId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
