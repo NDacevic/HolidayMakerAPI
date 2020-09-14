f98c0ab6a71ecfca89a3b9c0d94d08e7c511a171
mikaelollhage <mikaelollhage@gmail.com>
2020-09-14 10:41:39 +0200

    SearchParameterDto class added

---
+++ HolidayMakerAPI/HolidayMakerAPI/DTO/SearchParameterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolidayMakerAPI.DTO
+{
+    public class SearchParameterDto
+    {
+        public string City { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int NumberOfGuests { get; set; }
+
+
+
+    }
+}
