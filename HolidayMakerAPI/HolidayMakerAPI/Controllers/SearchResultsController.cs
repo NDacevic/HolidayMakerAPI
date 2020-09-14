d86f7009f56f3c330adb4b72f34c39d56843b577
mikaelollhage <mikaelollhage@gmail.com>
2020-09-14 14:03:36 +0200

    PostHome (GET) written

--- HolidayMakerAPI/HolidayMakerAPI/Controllers/SearchResultsController.cs
+++ HolidayMakerAPI/HolidayMakerAPI/Controllers/SearchResultsController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HolidayMakerAPI;
 using HolidayMakerAPI.Data;
+using HolidayMakerAPI.DTO;
+using HolidayMakerAPI.Models;
 
 namespace HolidayMakerAPI.Controllers
 {
@@ -77,13 +79,43 @@ namespace HolidayMakerAPI.Controllers
         // POST: api/SearchResults
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Home>> PostHome(Home home)
-        {
-            _context.Home.Add(home);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetHome", new { id = home.HomeId }, home);
+        //POST used as GET to be able to pass in an object
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Home>>> PostHome(SearchParameterDto searchParameters)
+        {
+            //Get all homes within the correct location and that has more beds in total than NumberOfGuests. Save to list.
+            List<Home> selectedHomes = await _context.Home.
+                Where(h => h.Location == searchParameters.Location &&
+                h.NumberOfBeds >= searchParameters.NumberOfGuests).ToListAsync();
+
+            //Loop through all relevant homes and check for conflicting reservations
+            for (int i = 0; i < selectedHomes.Count; i++)
+            {
+                //Recreate the list each iteration since only reservations per active home is relevant
+                List<Reservation> reservationsPerHome = new List<Reservation>();
+
+                //Get all reservations for the current iteration's home object where the reservations enddate is larger than the searches start date (only reservations that hasn't already passed)
+                reservationsPerHome = await _context.Reservation.Where(r => r.Home.HomeId == selectedHomes[i].HomeId && r.EndDate>=searchParameters.StartDate).ToListAsync();
+
+                for (int j = 0; j < reservationsPerHome.Count; j++)
+                {
+                    //if this home's current iteration's reservation's StartDate is BEFORE the Search's EndDate AND AFTER the Search's StartDate - then the Home is not available when wanted
+                    if ((reservationsPerHome[j].StartDate < searchParameters.EndDate && reservationsPerHome[j].StartDate > searchParameters.StartDate) ||
+                        //OR this home's current iteration's reservation's EndDate is AFTER the Search's StartDate AND BEFORE the Search's EndDate - then the Home is not available when wanted
+                        (reservationsPerHome[j].EndDate>searchParameters.StartDate && reservationsPerHome[j].EndDate<searchParameters.EndDate))
+                    {
+                        //The home is not available when wanted, remove from list
+                        selectedHomes.Remove(selectedHomes[i]);
+                        //go back one incrementation since one home has been removed
+                        i--;
+                        //break out of Reservation-loop, the home is not available anyway
+                        break;
+                    }
+                }
+            }
+
+            return selectedHomes;
         }
 
         // DELETE: api/SearchResults/5
--- HolidayMakerAPI/HolidayMakerAPI/DTO/SearchParameterDto.cs
+++ HolidayMakerAPI/HolidayMakerAPI/DTO/SearchParameterDto.cs
@@ -7,7 +7,7 @@ namespace HolidayMakerAPI.DTO
 {
     public class SearchParameterDto
     {
-        public string City { get; set; }
+        public string Location { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int NumberOfGuests { get; set; }
