23567f5de82d043938ec65bd0a13a6df371110e7
mikaelollhage <mikaelollhage@gmail.com>
2020-09-17 10:59:55 +0200

    Added LoginUsersController and GetUser()

---
+++ HolidayMakerAPI/HolidayMakerAPI/Controllers/LoginUsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HolidayMakerAPI;
+using HolidayMakerAPI.Data;
+
+namespace HolidayMakerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginUsersController : ControllerBase
+    {
+        private readonly HolidayMakerAPIContext _context;
+
+        public LoginUsersController(HolidayMakerAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LoginUsers/
+        [HttpGet("{email}")]
+        public async Task<ActionResult<User>> GetUser(string email)
+        {
+            var user = await _context.User.FindAsync(email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
